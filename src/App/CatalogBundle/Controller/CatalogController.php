ce80cefce0852b0cb5cf687d55a9c7c0ea9fd400
duker <duker33@gmail.com>
2015-04-27 13:28:48 +0300

    Правки в CatalogController

--- src/App/CatalogBundle/Controller/CatalogController.php
+++ src/App/CatalogBundle/Controller/CatalogController.php
@@ -111,6 +111,10 @@ class CatalogController extends Controller
         ));
     }
 
+    /**
+     * Поискиовая выдача списка продуктов с пагинацей
+     * @return Response
+     */
     public function searchAction()
     {
         $condition = $this->getRequest()->query->get('condition');
@@ -157,7 +161,7 @@ class CatalogController extends Controller
         return new Response($json);
     }
 
-    private  function isParent($category,$catUrl){
+    private function isParent($category, $catUrl){
 
         $catRp = $this->getDoctrine()->getRepository("AppCatalogBundle:Category");
         $parents = $catRp->getPath($category);
@@ -167,151 +171,4 @@ class CatalogController extends Controller
         }
         return false;
     }
-
-
-/*
-    public function migrate1Action()
-    {
-        $em = $this->getDoctrine()->getEntityManager();
-
-        $catArr = array(
-            537 => array('prom-stroy', 'Общегражданское и промышленное строительство'),
-            538 => array('dor-stroy', 'Дорожное строительство'),
-            539 => array('ingener-stroy', 'Строительство инженерных сетей'),
-            540 => array('energy-stroy', 'Строительство энергетических объектов'),
-            541 => array('blag-territory', 'Благоустройство территорий'),
-            542 => array('neftegaz-stroy', 'Нефтегазовое строительство')
-        );
-
-        foreach($catArr as $id => $data) {
-            $cat = new Category();
-            $cat->setId($id)
-                ->setName($data[1])
-                ->setTitle($data[1])
-                ->setAlias($data[0])
-                ->setCoefficient(1)
-                ->setIsActive(1);
-            $cat->setOrder(0);
-
-            $em->persist($cat);
-            $em->flush();
-            $em->clear();
-        }
-
-        die('success 1');
-    }
-
-    public function migrate2Action()
-    {
-        $baseCats = array(
-            537 => array('title' => 'Общегражданское и промышленное строительство', 'uri' => 'prom-stroy'),
-            538 => array('title' => 'Дорожное строительство','uri' =>  'dor-stroy'),
-            539 => array('title' => 'Строительство инженерных сетей','uri' => 'ingener-stroy'),
-            540 => array('title' => 'Строительство энергетических объектов','uri' => 'energy-stroy'),
-            541 => array('title' => 'Благоустройство территорий', 'uri' => 'blag-territory'),
-            542 => array('title' => 'Нефтегазовое строительство', 'uri' => 'neftegaz-stroy')
-        );
-
-        $catRp = $this->getDoctrine()->getRepository('AppCatalogBundle:Category');
-        $gbiRp = $this->getDoctrine()->getRepository('AppCatalogBundle:Gbi');
-        $em = $this->getDoctrine()->getEntityManager();
-        $gbis= $gbiRp->findAllOrderedBy('parentId', 'ASC');
-
-        foreach($gbis as $gbi) {
-            $cat = new Category();
-            $cat->setId($gbi->getId())
-                ->setName($gbi->getNameContent())
-                ->setTitle($gbi->getName())
-                ->setAlias($gbi->getAlias())
-                ->setMark($gbi->getWorkDocs())
-                ->setDescription($gbi->getDesc())
-                ->setCoefficient($gbi->getKoefPrice())
-                ->setIsActive(1);
-
-            $gbiParentId = $gbi->getParentId();
-            $gbiOrder = $gbi->getOrder();
-
-            if(!empty($gbiParentId)) {
-                $cat->setParent($catRp->find($gbi->getParentId()));
-            } else {
-                $gbiUriParent = $gbi->getUriParent();
-                foreach($baseCats as $id => $baseCat) {
-                    if($baseCat['uri'] == $gbiUriParent) {
-                        $cat->setParent($catRp->find($id));
-                    }
-                }
-            }
-
-            if(!empty($gbiOrder)) {
-                $cat->setOrder($gbi->getOrder());
-            } else {
-                $cat->setOrder(0);
-            }
-
-            $em->persist($cat);
-            $em->flush();
-            $em->clear();
-        }
-        die('success');
-    }
-
-    public function migrate3Action()
-    {
-        $catRp = $this->getDoctrine()->getRepository('AppCatalogBundle:Category');
-        $gbiRp = $this->getDoctrine()->getRepository('AppCatalogBundle:Gbi');
-        $em = $this->getDoctrine()->getEntityManager();
-        $gbis= $gbiRp->findAllOrderedBy('parentId', 'ASC');
-
-        foreach($gbis as $gbi) {
-            $cat = $catRp->find($gbi->getId());
-            $gbiParentId = $gbi->getParentId();
-
-            if(!empty($gbiParentId)) {
-                $cat->setParent($catRp->find($gbiParentId));
-            }
-
-            $em->persist($cat);
-            $em->flush();
-            $em->clear();
-        }
-        die('success too');
-    }
-
-    /*
-    public function exploreSectionAction($catUrl, $sectionId)
-    {
-        // search category ID
-        $catId = array_search($catUrl, $this->baseCats);
-
-        $catRp = $this->getDoctrine()->getRepository('AppCatalogBundle:Category');
-        $section = $catRp->find($sectionId);
-        $parents = $catRp->getPath($section);
-
-        $hierarchyOptions = array(
-            'childSort' => array(
-                'field' => 'title',
-                'dir'   => 'asc'
-            )
-        );
-
-        $childs = $catRp->buildTreeArray($catRp->getNodesHierarchy($section, false, $hierarchyOptions));
-
-        if(!empty($childs)) {
-            return $this->render('AppCatalogBundle:Default:category.explore.html.twig', array(
-                'parents'   => $parents,
-                'childs'    => $childs,
-                'catUrl'    => $this->baseCats[$catId],
-                'category'  => $section
-            ));
-        } else {
-            $products = $
-            return $this->render('AppCatalogBundle:Default:section.explore.html.twig', array(
-                'parents'  => $parents,
-                'childs'   => $childs,
-                'catUrl'   => $this->baseCats[$catId],
-                'section'  => $section,
-                'products' => $products
-            ));
-        }
-    }*/
 }
