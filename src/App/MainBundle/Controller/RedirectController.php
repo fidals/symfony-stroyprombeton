ed35a3db3020a1233dea3a279038fd72b551ba7e
cybervito21 <cybervito21@gmail.com>
2015-07-10 18:16:05 +0300

    [ 286 ] + Роутинг для gbi visual + Роутинг для staticpage + Роутинг для territory

---
+++ src/App/MainBundle/Controller/RedirectController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RedirectController extends Controller
+{
+	public function redirectAction()
+	{
+
+	}
+}
