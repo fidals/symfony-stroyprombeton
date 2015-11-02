a95a423994617e437654cd58c68bb5204e87a0e3
YozhEzhi <yozhezhi@gmail.com>
2015-11-02 13:02:44 +0400

    [ 394 ] Приложить файл -> Прикрепить файл

--- src/App/CatalogBundle/Controller/CartController.php
+++ src/App/CatalogBundle/Controller/CartController.php
@@ -63,43 +63,33 @@ class CartController extends Controller
 
 			if ($form->isValid()) {
 				$mailer = $this->get('mailer');
-
 				$recipients = array(
 					$this->container->getParameter('email_info'),
 					$form['email']->getData()
 				);
-
 				$body = $this->renderView('AppCatalogBundle:Cart:email.order.html.twig', array(
 					'form' => $form->createView(),
 					'cart' => $this->get('catalog.cart')->loadCart(true)
 				));
-
 				$message = \Swift_Message::newInstance()
 					->setSubject('Stroyprombeton | New order')
 					->setTo($recipients)
 					->setFrom($this->container->getParameter('email_order'))
 					->setContentType("text/html")
 					->setBody($body);
-
 				$file = $form['file']->getData();
 
 				if ($file) {
 					$fs = new Filesystem();
-
-					$filePath     = 'tmp/';
-					$fileName     = $file->getClientOriginalName();
+					$filePath = 'tmp/';
+					$fileName = $file->getClientOriginalName();
 					$fileFullPath = $filePath . $fileName;
-
 					$file->move($filePath, $fileName);
-
 					$message->attach(\Swift_Attachment::fromPath($fileFullPath));
-
 					$mailer->send($message);
-
 					$transport = $this->container->get('mailer')->getTransport();
 					$spool = $transport->getSpool();
 					$spool->flushQueue($this->container->get('swiftmailer.transport.real'));
-
 					$fs->remove($fileFullPath);
 				} else {
 					$mailer->send($message);
